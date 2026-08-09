0f7066f2fc8b9cb8d9e882cdcbd52227c5f55450
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-09 17:45:00 +0300

    create search input component

--- src/HeroSection/HeroSection.tsx
+++ src/HeroSection/HeroSection.tsx
@@ -1,9 +1,12 @@
 import type {FC} from "react";
+import SearchInput from "../components/SearchInput/SearchInput.tsx";
 
 const HeroSection: FC = () => {
     return (
         <main className='hero-section'>
             <h1 className='hero-heading'>Search everything...</h1>
+
+            <SearchInput />
         </main>
     )
 }
---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,41 @@
+import type {FC} from "react";
+
+const SearchInput: FC = () => {
+    return (
+        <div className="search">
+            <svg className="search__icon" viewBox="0 0 24 24" fill="none">
+                <path
+                    d="M9 18.5V6.5L21 4V16.5"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+                <circle
+                    cx="6"
+                    cy="18"
+                    r="3"
+                    stroke="currentColor"
+                    stroke-width="2"
+                />
+                <circle
+                    cx="18"
+                    cy="16"
+                    r="3"
+                    stroke="currentColor"
+                    stroke-width="2"
+                />
+            </svg>
+
+            <input
+                className="search__input"
+                type="search"
+                placeholder="Search music..."
+            />
+
+            <kbd className="search__shortcut">X</kbd>
+        </div>
+    )
+}
+
+export default SearchInput
