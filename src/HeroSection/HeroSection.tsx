c090ee3b4e72a906a0c900f062de93ddc832ab4f
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-09 17:38:52 +0300

    create hero section

---
+++ src/HeroSection/HeroSection.tsx
@@ -0,0 +1,11 @@
+import type {FC} from "react";
+
+const HeroSection: FC = () => {
+    return (
+        <main className='hero-section'>
+            <h1 className='hero-heading'>Search everything...</h1>
+        </main>
+    )
+}
+
+export default HeroSection
--- src/components/MusicPlayer/MusicPlayer.tsx
+++ src/components/MusicPlayer/MusicPlayer.tsx
@@ -1,6 +1,7 @@
 import {type FC, useEffect, useState} from "react";
 import './MusicPlayer.css'
 import Intro from "../Intro/Intro.tsx";
+import HeroSection from "../../HeroSection/HeroSection.tsx";
 
 const MusicPlayer: FC = () => {
     const isIntro: string = localStorage.getItem('isIntro') || 'false'
@@ -24,6 +25,8 @@ const MusicPlayer: FC = () => {
             <p className={`initial-text ${startSite || isIntro === 'true' ? 'initial-text-hide' : ''}`}>Click to start</p>
 
             <Intro isStartSite={startSite} isIntro={isIntro}/>
+
+            <HeroSection />
         </div>
     )
 }
