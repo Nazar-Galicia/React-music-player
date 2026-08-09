c7604ac535ac77e6ae782ecdaec9b07fc29e8707
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-09 17:04:12 +0300

    logic for playing sounds

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,5 +1,8 @@
 import {type FC, useEffect, useRef} from "react";
 import './Intro.css'
+import whisperVoice from '@/assets/sounds/whisper-listen-everywhere.mp3'
+import boomSwoosh from '@/assets/sounds/boom-swoosh.mp3'
+import shortBass from '@/assets/sounds/short-bass.mp3'
 
 interface IntroProps {
     isStartSite: boolean;
@@ -12,6 +15,10 @@ const Intro: FC<IntroProps> = (props) => {
         isIntro,
     } = props
 
+    const whisper: HTMLAudioElement | null = new Audio(whisperVoice)
+    const boom: HTMLAudioElement | null = new Audio(boomSwoosh)
+    const bass: HTMLAudioElement | null = new Audio(shortBass)
+
     const listenRef = useRef<HTMLSpanElement | null>(null)
     const everywhereRef = useRef<HTMLSpanElement | null>(null)
 
@@ -19,9 +26,15 @@ const Intro: FC<IntroProps> = (props) => {
         if (isStartSite === true && isIntro === 'false') {
             setTimeout(() => {
                 listenRef.current?.classList.remove('intro-word-hidden')
+                whisper?.play()
+                bass?.play()
 
                 setTimeout(() => {
                     everywhereRef.current?.classList.remove('intro-word-hidden')
+
+                    setTimeout(() => {
+                        boom?.play()
+                    }, 1000)
 
                     setTimeout(() => {
                         listenRef.current?.classList.add('intro-word-hidden')
