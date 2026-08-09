063d5f5c03a6d45e99349547e1b61e465567a29b
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-09 16:21:43 +0300

    add style logic for hiding initial text after clicking

--- src/components/MusicPlayer/MusicPlayer.tsx
+++ src/components/MusicPlayer/MusicPlayer.tsx
@@ -1,12 +1,12 @@
-import {type FC, useEffect} from "react";
+import {type FC, useEffect, useRef} from "react";
 import './MusicPlayer.css'
 
 const MusicPlayer: FC = () => {
     const isIntro: string = localStorage.getItem('isIntro') || 'false'
-    let startSite: boolean = false
+    const startSite = useRef<boolean>(false)
 
     const startIntro = (): void => {
-        startSite = true
+        startSite.current = true
         document.removeEventListener('click', startIntro)
     }
 
@@ -20,7 +20,7 @@ const MusicPlayer: FC = () => {
 
     return (
         <div className='music-player'>
-            <p className='initial-text'>Click to start</p>
+            <p className={`initial-text ${startSite.current ? 'initial-text-hide' : ''}`}>Click to start</p>
         </div>
     )
 }
