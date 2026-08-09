72f296d11912f8d45c6add6fc29a97df081ac273
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-09 16:18:19 +0300

    add logic for starting intro and site

--- src/components/MusicPlayer/MusicPlayer.tsx
+++ src/components/MusicPlayer/MusicPlayer.tsx
@@ -1,7 +1,23 @@
-import type {FC} from "react";
+import {type FC, useEffect} from "react";
 import './MusicPlayer.css'
 
 const MusicPlayer: FC = () => {
+    const isIntro: string = localStorage.getItem('isIntro') || 'false'
+    let startSite: boolean = false
+
+    const startIntro = (): void => {
+        startSite = true
+        document.removeEventListener('click', startIntro)
+    }
+
+    useEffect(() => {
+        if (isIntro === 'false') {
+            document.addEventListener('click', startIntro)
+        }
+
+        return () => document.removeEventListener('click', startIntro)
+    }, [])
+
     return (
         <div className='music-player'>
             <p className='initial-text'>Click to start</p>
