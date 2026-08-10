6b2198e3829b403299bae92399eba5d93a77eab6
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-10 11:26:15 +0300

    add request to get music meta data

--- src/api/musicMetaAPI.ts
+++ src/api/musicMetaAPI.ts
@@ -2,6 +2,6 @@ const URL: string = 'https://itunes.apple.com/search'
 
 export const musicMetaAPI = {
     getSongData: async (query: string) => {
-        return fetch(`${URL}?terms=${query}&entity=song&limit=10`).then(res => res.json())
+        return fetch(`${URL}?term=${query}&entity=song&limit=10`).then(res => res.json())
     }
 }
--- src/components/TracksList/TracksList.tsx
+++ src/components/TracksList/TracksList.tsx
@@ -1,4 +1,5 @@
-import {type FC, useEffect} from "react";
+import {type FC, useEffect, useState} from "react";
+import {musicMetaAPI} from "../../api/musicMetaAPI.ts";
 
 interface TracksListProps {
     query: string;
@@ -8,6 +9,19 @@ const TracksList: FC<TracksListProps> = (props) => {
     const {
         query,
     } = props
+
+    const [tracks, setTracks] = useState(null)
+
+    useEffect(() => {
+        console.log(query)
+        musicMetaAPI.getSongData(query).then(data => {
+            setTracks(data)
+        })
+    }, [query]);
+
+    useEffect(() => {
+        console.log(tracks)
+    }, [tracks]);
 
     return (
         <div className='tracks-list'></div>
