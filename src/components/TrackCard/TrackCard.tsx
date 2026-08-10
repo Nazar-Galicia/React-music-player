8555e31ff350bac3be1be7054928d1bdf87f0e83
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-10 14:19:06 +0300

    removed track card to component, add render track list

---
+++ src/components/TrackCard/TrackCard.tsx
@@ -0,0 +1,43 @@
+import type {FC} from "react";
+import './TrackCard.css'
+
+interface TrackCardProps {
+    image: string,
+    title: string,
+    artist: string,
+    duration: string,
+}
+
+const TrackCard: FC<TrackCardProps> = (props) => {
+    const {
+        image,
+        title,
+        artist,
+        duration,
+    } = props
+
+    return (
+        <li className="music-card">
+            <img
+                className="music-card__cover"
+                src={image}
+                alt="Album cover"
+            />
+
+            <div className="music-card__info">
+                <h3 className="music-card__title">{title}</h3>
+                <p className="music-card__artist">{artist}</p>
+            </div>
+
+            <span className="music-card__duration">{duration}</span>
+
+            <button className="music-card__play" aria-label="Play">
+                <svg viewBox="0 0 24 24">
+                    <path d="M8 5v14l11-7z"/>
+                </svg>
+            </button>
+        </li>
+    )
+}
+
+export default TrackCard;
--- src/components/TracksList/TracksList.tsx
+++ src/components/TracksList/TracksList.tsx
@@ -1,6 +1,7 @@
 import {type FC, useEffect, useState} from "react";
 import {musicMetaAPI} from "../../api/musicMetaAPI.ts";
 import './TrackList.css'
+import TrackCard from "../TrackCard/TrackCard.tsx";
 
 interface TracksListProps {
     query: string;
@@ -26,46 +27,16 @@ const TracksList: FC<TracksListProps> = (props) => {
 
     return (
         <ul className='tracks-list'>
-            <li className="music-card">
-                <img
-                    className="music-card__cover"
-                    src="https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=200"
-                    alt="Album cover"
-                />
-
-                <div className="music-card__info">
-                    <h3 className="music-card__title">Blinding Lights</h3>
-                    <p className="music-card__artist">The Weeknd</p>
-                </div>
-
-                <span className="music-card__duration">3:20</span>
-
-                <button className="music-card__play" aria-label="Play">
-                    <svg viewBox="0 0 24 24">
-                        <path d="M8 5v14l11-7z"/>
-                    </svg>
-                </button>
-            </li>
-            <li className="music-card">
-                <img
-                    className="music-card__cover"
-                    src="https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=200"
-                    alt="Album cover"
-                />
-
-                <div className="music-card__info">
-                    <h3 className="music-card__title">Blinding Lights</h3>
-                    <p className="music-card__artist">The Weeknd</p>
-                </div>
-
-                <span className="music-card__duration">3:20</span>
-
-                <button className="music-card__play" aria-label="Play">
-                    <svg viewBox="0 0 24 24">
-                        <path d="M8 5v14l11-7z"/>
-                    </svg>
-                </button>
-            </li>
+            {
+                tracks && tracks.results.map(song => (
+                    <TrackCard
+                        image={song.artworkUrl100}
+                        title={song.trackName}
+                        artist={song.artistName}
+                        duration={song.trackTimeMillis}
+                    />
+                ))
+            }
         </ul>
     )
 }
