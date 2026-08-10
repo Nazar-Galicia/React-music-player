3833118e7dd64be70f19d04ff1afd393cd67f750
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-10 17:42:05 +0300

    add logic for spawning and moving bg lines

--- src/components/MusicBackgroundLine/MusicBackgroundLine.tsx
+++ src/components/MusicBackgroundLine/MusicBackgroundLine.tsx
@@ -1,8 +1,76 @@
-import type {FC} from "react";
+import {type FC, useEffect, useRef} from "react";
 
-const MusicBackgroundLine: FC = () => {
+interface MusicBackgroundLineProps {
+    lineObj: {
+        id: string,
+        brightness: number,
+        fontSize: number,
+        line: string,
+        speedY: number,
+        spX: number,
+        spY: number,
+    },
+    setExitLine(id: string): void,
+}
+
+const MusicBackgroundLine: FC<MusicBackgroundLineProps> = (props) => {
+    const {
+        lineObj,
+        setExitLine
+    } = props
+
+    const {
+        id,
+        brightness,
+        fontSize,
+        line,
+        speedY,
+        spX,
+        spY,
+    } = lineObj
+
+    const y = useRef(0)
+    const lineRef = useRef(null)
+
+    const changeCords = () => {
+        if (y.current > window.innerHeight + 100) {
+            setExitLine(id)
+        } else {
+            y.current += speedY * 0.05
+
+            if (lineRef.current) {
+                lineRef.current.style.transform = `translateY(${y.current}px)`
+            }
+        }
+        requestAnimationFrame(changeCords)
+    }
+
+    useEffect(() => {
+        let frameId: number;
+
+        const animate = () => {
+            changeCords();
+            frameId = requestAnimationFrame(animate);
+        };
+
+        frameId = requestAnimationFrame(animate);
+
+        return () => cancelAnimationFrame(frameId);
+    }, []);
+
     return (
-        <span className='music-bacground-line'></span>
+        <span
+            ref={lineRef}
+            className='music-bacground-line'
+            style={{
+                fontSize: `${fontSize}px`,
+                filter: `brightness(${brightness})`,
+                left: `${spX}px`,
+                top: `${spY}px`,
+            }}
+        >
+            {line}
+        </span>
     )
 }
 
