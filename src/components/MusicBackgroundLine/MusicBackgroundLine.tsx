eaa604f228d2085591b0dc49646ddebbd6a439ba
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-10 17:31:21 +0300

    create component for bg line

---
+++ src/components/MusicBackgroundLine/MusicBackgroundLine.tsx
@@ -0,0 +1,9 @@
+import type {FC} from "react";
+
+const MusicBackgroundLine: FC = () => {
+    return (
+        <span className='music-bacground-line'></span>
+    )
+}
+
+export default MusicBackgroundLine;
