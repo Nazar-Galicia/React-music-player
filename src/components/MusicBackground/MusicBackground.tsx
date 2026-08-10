63be7078876610e234ba86628749c6526128222a
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-10 17:44:04 +0300

    add logic for spawning and maping lines in bg container

--- src/components/MusicBackground/MusicBackground.tsx
+++ src/components/MusicBackground/MusicBackground.tsx
@@ -1,8 +1,21 @@
 import type {FC} from "react";
+import {useMusicBackground} from "../../hooks/useMusicBackground.ts";
+import MusicBackgroundLine from "../MusicBackgroundLine/MusicBackgroundLine.tsx";
 
 const MusicBackground: FC = () => {
+    const {
+        lines,
+        setExitLine,
+    } = useMusicBackground()
+
     return (
-        <div className='music-background'></div>
+        <div className='music-background'>
+            {
+                lines?.map((line) => (
+                    <MusicBackgroundLine lineObj={line} setExitLine={setExitLine} />
+                ))
+            }
+        </div>
     )
 }
 
