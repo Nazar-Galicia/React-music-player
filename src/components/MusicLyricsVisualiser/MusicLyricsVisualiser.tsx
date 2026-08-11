916fc236569e4e323a1686c747e7e719eab29113
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-11 12:04:32 +0300

    create lyrics visualiser component

---
+++ src/components/MusicLyricsVisualiser/MusicLyricsVisualiser.tsx
@@ -0,0 +1,9 @@
+import type {FC} from "react";
+
+const MusicLyricsVisualiser: FC = () => {
+    return (
+        <div className='music-lyrics-visualisator'></div>
+    )
+}
+
+export default MusicLyricsVisualiser
