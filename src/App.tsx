a05acebbc02671e7977743c6dd29e5abc9d334e5
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-23 12:34:37 +0300

    change toaster markup. now it inside router

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,10 @@ import '@/styles/reset.css'
 import '@/styles/fonts.css'
 import '@/styles/global.css'
 import Router from "./Router.tsx";
-import Toast from "./components/Toast/Toast.tsx";
-import ToasterProvider from "./context/ToasterContext.tsx";
 
 const App: FC = () => {
   return (
-      <ToasterProvider>
-          <Toast />
-          <Router />
-      </ToasterProvider>
+      <Router />
   )
 }
 
--- src/Router.tsx
+++ src/Router.tsx
@@ -3,14 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home.tsx";
 import Visualiser from "./pages/Visualiser/Visualiser.tsx";
+import ToasterProvider from "./context/ToasterContext.tsx";
+import Toast from "./components/Toast/Toast.tsx";
 
 const Router: FC = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />}/>
-                <Route path='/visualiser/:artist/:trackName' element={<Visualiser />}/>
-            </Routes>
+            <ToasterProvider>
+                <Toast />
+                <Routes>
+                    <Route path='/' element={<Home />}/>
+                    <Route path='/visualiser/:artist/:trackName' element={<Visualiser />}/>
+                </Routes>
+            </ToasterProvider>
         </BrowserRouter>
     )
 }
